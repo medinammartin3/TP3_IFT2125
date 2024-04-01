7bbaef7107115fc6db9ac84a9ca755d820805aa3
Martin <medinammartin3@gmail.com>
2024-04-01 15:29:55 -0400

    Initial code

---
+++ Q4_Labyrinthe/labyrinth_generator_creator.py
@@ -0,0 +1,75 @@
+import sys
+import random
+
+cell_size = 10 #mm
+wall_height = 10 #mm
+wall_thickness = 1 #mm
+
+strategy_choice = 1
+
+class Strategy :
+    def __init__(self):
+        pass
+
+    def Apply(self):
+        print("Applying Abstract Strategy")
+
+    def DoSomething(self):
+        print("Do Something")
+
+class Algorithm1(Strategy) :
+    def Apply(self):
+        #super().Apply()
+        print("Applying Algorithm1")
+
+class Algorithm2(Strategy) :
+
+    def Apply(self):
+        #super().Apply()
+        print("Applying Algorithm2")
+
+class Generator() :
+    strategy = None
+
+    def __init__(self):
+        pass
+
+    def SetStrategy(self, new_strategy):
+        self.strategy = new_strategy
+
+    def Generate(self):
+        self.strategy.Apply()
+        self.strategy.DoSomething()
+
+class Creator() :
+    def __init__(self):
+        pass
+
+    def PrintLabyrinth(self):
+        pass
+
+
+# main call
+def main():
+    global strategy_choice
+    args = sys.argv[:]
+    if len(args) >= 2 :
+        strategy_choice = int(args[1])
+
+    # Generator
+    my_generator = Generator()
+    if strategy_choice == 1:
+        my_generator.SetStrategy(Algorithm1())
+    elif strategy_choice == 2:
+        my_generator.SetStrategy(Algorithm2())
+    else :
+        print("error strategy choice")
+    my_generator.Generate()
+
+    #Creator
+    my_creator = Creator()
+    my_creator.PrintLabyrinth()
+
+
+if __name__ == "__main__":
+    main()
